50ca392a7bcd6be0636ecc71d391733776abb6e6
Darlesson Oliveira <darlesson@gmail.com>
2014-05-06 15:52:25 -0700

    Add jQuery.fn.outerHTML as part of the extensions

--- src/jquery.extended.js
+++ src/jquery.extended.js
@@ -326,6 +326,81 @@
             }).length;
         },
 
+        outerHTML: function(value){
+            /// <summary>
+            ///     Returns the outerHTML for the first element matched, replaces the all elements matched by another
+            ///     returning the element in the chain, or allow value as Function to let the developer to decide
+            ///     what to do with every element in the object.
+            /// </summary>
+            /// <param name="value">
+            ///     A HTML String, Element or Function.
+            /// </param>
+            /// <returns>
+            ///     Either a HTML String or the jQuery instance.
+            /// </returns>
+
+            // If there is no element in the jQuery object
+            if(!this.length)
+                return null;
+
+            // Returns the value
+            else if(value === undefined){
+
+                var element = this[0],
+                    result;
+
+                // Return browser outerHTML (Most newer browsers support it)
+                if(element.outerHTML)
+                    result = element.outerHTML;
+
+                // Return it using the jQuery solution
+                else
+                    result = $(document.createElement("div")).append($(element).clone()).html();
+
+                // Trim the result
+                if(typeof result === "string")
+                    result = $.trim(result);
+
+                return result;
+
+            }
+            // Deal with function
+            else if( $.isFunction(value) ){
+
+                this.each(function(i){
+                    var $this = $( this );
+                    $this.outerHTML( value.call(this, i, $this.outerHTML()) );
+                });
+
+            }
+            // Replaces the content
+            else {
+
+                var $this = $(this),
+                    replacingElements = [],
+                    $value = $(value),
+                    $cloneValue;
+
+                for(var x = 0; x < $this.length; x++){
+
+                    // Clone the value for each element being replaced
+                    $cloneValue = $value.clone(true);
+
+                    // Use jQuery to replace the content
+                    $this.eq(x).replaceWith($cloneValue);
+
+                    // Add the replacing content to the collection
+                    for(var i = 0; i < $cloneValue.length; i++)
+                        replacingElements.push($cloneValue[i]);
+
+                }
+
+                // Return the replacing content if any
+                return (replacingElements.length) ? $(replacingElements) : null;
+
+            }
+        },
+
         reverse: [].reverse
 
     });
